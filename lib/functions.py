dfabcb8acf2cf6bac6f7be011a8edbaeffc57dc2
lakota <lakotajcamp@gmail.com>
2023-12-11 01:06:47 -0500

    lab complete

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,24 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
-    pass
+    print("Hello, programmer!")
 
-def greet(name):
-    pass
+def greet(name="Naureen"):
+    print(f"Hello, {name}!")
 
 def greet_with_default(name="programmer"):
-    pass
+    print(f"Hello, {name}!")
+greet_with_default("Sunny")
 
 def add(num1, num2):
-    pass
+    print(num1 + num2)
+    return num1 + num2
+sum = add(1, 2)
+print(sum)
 
 def halve(number):
-    pass
+
+  return number / 2
+
+result = halve(4)
+print(result)
